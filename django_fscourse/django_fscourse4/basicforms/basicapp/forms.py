7eeb35e7a4a96a66afc36a6b6a2740ca0c9c89ac
Londoncomputadores <30436251+Londoncomputadores@users.noreply.github.com>
2018-07-31 00:42:16 -0300

    Add files via upload

---
+++ django_fscourse/django_fscourse4/basicforms/basicapp/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.core import validators
+
+class FormName(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email!')
+    text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError('Make Sure Emails Match!')
+
+#segundo exemplo explicado e depois modificado. veja acima!
+'''def check_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError('Name Needs To Start With Z!')
+
+class FormName(forms.Form):
+    name = forms.CharField()
+    email = forms.CharField()
+    text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput,
+                                                 validators=[validators.MaxLengthValidator(0)])
+
+    #Primeiro exemplo de validators explicado e depois apagado
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher) > 0:
+            raise forms.ValidationError('Gotcha Bot!')
+        return botcatcher
+    '''
